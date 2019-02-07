e034bcaae1aa4be687092736520a2cd8f92d1cc8
martintroye <troyemartin@gmail.com>
2019-02-06 21:18:27 -0600

    Primitive types

---
+++ week-1/martin-assignment_1_4.js
@@ -0,0 +1,37 @@
+/*
+============================================
+; Title: Assignment 1.4
+; Author: Troy Martin
+; Date: 02/06/2019
+; Modified By: Troy Martin
+; Description: Primitive types
+;===========================================
+*/
+
+// start program
+
+/*
+; Expected output:
+; Display first name, last name, age, address, city and zip code
+*/
+
+// variable declaration and assignment goes here...
+var firstName = "Troy";
+var lastName = "Martin";
+var age = "50";
+var street = "12345 Main St";
+var city = "Omaha";
+var state = "NE";
+var zipCode = "68521";
+
+
+// output
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(street);
+console.log(city);
+console.log(state);
+console.log(zipCode);
+
+// end program

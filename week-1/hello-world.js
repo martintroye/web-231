5375afb4307ee59a0417948da2aaa32c65f65e8b
martintroye <troyemartin@gmail.com>
2019-02-06 19:18:06 -0600

    Demo of running JavaScript in VS Code

---
+++ week-1/hello-world.js
@@ -0,0 +1,10 @@
+/*
+============================================
+; Title: Exercise 1.3
+; Author: Troy Martin
+; Date: 02/06/2019
+; Modified By: Troy Martin
+; Description: Demo of running JavaScript in Visual Studio Code.
+;===========================================
+*/
+console.log("Hello World, my name is Troy Martin.");

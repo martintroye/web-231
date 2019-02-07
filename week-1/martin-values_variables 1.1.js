6185fc40150fa3da3e19f107e2c7a0250b9f02c3
martintroye <troyemartin@gmail.com>
2019-02-06 21:04:12 -0600

    Rename file to follow naming convention

---
+++ week-1/martin-values_variables 1.1.js
@@ -0,0 +1,34 @@
+/*
+============================================
+; Title: Values and Variables
+; Author: Troy Martin
+; Date: 02/06/2019
+; Modified By: Troy Martin
+; Description: Create a simple JavaScript program using values and variables with at least two errors. 
+; The program should not execute and there should be at least two errors in the code.
+;===========================================
+*/
+
+// start program
+
+/*
+; Expected output:
+; Calculate and display the total cost based on the cost per hour, number of hours and the number of users
+*/
+
+// variable declaration and assignment goes here...
+costPerHour;
+var numberOfHours;
+var numberOfUsers;
+
+costPerHour = 250.25;
+numberOfHours = 10;
+numberOfUsers = 5;
+
+var  totalCost = numberOfUsers
+numberOfHours * costPerHour; 
+
+// output
+console.lo("Total cost: " + totalCost);
+
+// end program

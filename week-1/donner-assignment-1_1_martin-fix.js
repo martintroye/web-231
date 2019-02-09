bf4ab3579fa94fdd19a6e1aa349bc317ecd7163b
Troy Martin <troyemartin@gmail.com>
2019-02-09 11:22:42 -0600

    Fix comments

--- week-1/donner-assignment-1_1_martin-fix.js
+++ week-1/donner-assignment-1_1_martin-fix.js
@@ -30,7 +30,7 @@ const multiplication = number1*number2;
 const division = number1/number2;
 
 // output
-// martin - the new line included before multiplication is output has a backslah instead of a forward slash causing the lines to run together
+// martin - the new line included before multiplication in the output has a back slash instead of a forward slash causing the lines to run together
 console.log ('\n','6 + 3 = ' + addition, '\n','6 - 3 = ' + subtraction, '\n','6 * 3 = ' + multiplication, '\n','6 / 3 = ' + division)
 
 // end program

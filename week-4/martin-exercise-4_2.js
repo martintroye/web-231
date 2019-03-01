43f610b2c01bc63fe4866cda2a7c73be518018fc
Troy Martin <troyemartin@gmail.com>
2019-02-28 20:04:59 -0600

    Excercise 4.2 arrays, output the values from an array

--- week-4/martin-exercise-4_2.js
+++ week-4/martin-exercise-4_2.js
@@ -35,23 +35,29 @@ var fruits = ["Apple", "Orange", "Banana", "Mango", "Pear"];
 // function declaration
 
 /*
-; Params: fruit
+; Params: list
 ; Response: undefined
 ; Description: Log a message to the console for each item in the array.
 */
-function getFruit(fruits){
-    // Call the forEach function on the Array to loop each element in the array
-    fruits.forEach(item => {
-        // Call the console.log function to output the value of the item in the array
-        console.log(item)
-    });
+function getFruit(list){
+  // Using the isArray function make sure that the parameter is an Array
+  if(Array.isArray(list) !== true){
+    // If not an Array throw an error
+    throw Error("The parameter list, must be a valid array.")
+  }
+
+  // Call the forEach function from the Array prototype to loop the array
+  list.forEach(item => {
+      // Call the console.log function to output the value of the item in the array
+      console.log(item)
+  });
 }
 
 // output
 // Call the console.log() function and output a well-formatted header with a line feed
 console.log(header.display("Troy", "Martin", "Exercise 4.2"), "\n");
 
-// Call the getFruit function to output the values of the fruit array
+// Call the getFruit function to output the values of the fruits array
 getFruit(fruits);
 
 // end program

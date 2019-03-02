f80e5599da192a7c691f56356fc15201b7856544
Troy Martin <troyemartin@gmail.com>
2019-03-02 10:06:53 -0600

    Exercise 4.3 use conditional to filter an array

--- week-4/martin-exercise-4_3.js
+++ week-4/martin-exercise-4_3.js
@@ -10,17 +10,65 @@
 // start program
 // Require statement that imports the martin-header.js file from my root directory.
 const header = require("../header.js");
+
+/*
+;  Expected output:
+;
+; Troy Martin
+; Exercise 4.3
+; Today's Date
+;
+; -- DISPLAYING ARRAY ITEMS --
+; Car
+; Truck
+; Motorcycle
+; Airplane
+; Bus
+;
+; -- SELECTED VALUE --
+; Motorcycle
+;
+; -- SELECTED VALUE --
+; Bus
+*/
+
+
+// variable declaration and assignment
+// Declare an array to hold a list of vehicles.
+const vehicles = ["Car", "Truck", "Motorcycle", "Airplane", "Bus"]
+
+// function declaration
+
+/*
+; Params: list - an array, value - a string
+; Response: undefined
+; Description: Function loops through the list array and outputs the matching value if found.
+*/
+function getValue(list, value){
+  // Use the Array isArray method to validate that the list is an array before trying to iterate
+  if(Array.isArray(list) === true){
+    // Use the Array foreach method to iterate over the list of vehicles
+    list.forEach(element => {
+      // Conditional statement to compare the value parameter to the vehicle in the array
+      if(element === value){
+        // Call the console.log method to output the matching vehicle
+        console.log(element);
+      }
+    });
+  }
+}
+
+// output
 // Call the console.log() function and output a well-formatted header with a line feed
 console.log(header.display("Troy", "Martin", "Exercise 4.3"), "\n");
 
-/*
-; Expected output:
-; <Your expected output here>
-*/
+console.log("-- SELECTED VALUE --")
+// Call the getValue function to find a match
+getValue(vehicles, "Motorcycle");
 
-// variable declaration and assignment goes here...
-
-// output
-console.log("<output>");
+console.log("\n");
+console.log("-- SELECTED VALUE --")
+// Call the getValue function to find a match
+getValue(vehicles, "Bus");
 
 // end program
